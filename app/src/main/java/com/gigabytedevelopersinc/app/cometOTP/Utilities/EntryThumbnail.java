a2652784ae184f3b2ad302a3f77826b413c727cd
Emmanuel Nwokoma <nwokoma@gigabytedevelopersinc.com>
2020-05-19 02:40:11 +0100

    Added account thumbnail for No Starch Press
    Signed-off-by: Emmanuel Nwokoma <nwokoma@gigabytedevelopersinc.com>

--- app/src/main/java/com/gigabytedevelopersinc/app/cometOTP/Utilities/EntryThumbnail.java
+++ app/src/main/java/com/gigabytedevelopersinc/app/cometOTP/Utilities/EntryThumbnail.java
@@ -172,6 +172,7 @@ public class EntryThumbnail {
         NewEgg(R.drawable.thumb_newegg),
         NextCloud(R.drawable.thumb_nextcloud),
         Nintendo(R.drawable.thumb_nintendo),
+        NoStarchPress(R.drawable.thumb_no_starch_press),
         NPM(R.drawable.thumb_npm),
         Oculus(R.drawable.thumb_oculus),
         Office(R.drawable.thumb_office),
