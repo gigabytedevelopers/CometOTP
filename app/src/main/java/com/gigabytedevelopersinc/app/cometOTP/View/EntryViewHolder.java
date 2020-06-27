ad5b21b57801a1c889aaccc550a7119e0eb14b82
Emmanuel Nwokoma <nwokoma@gigabytedevelopersinc.com>
2020-06-27 05:55:47 +0100

    final UI update for accessibility improvement
    Signed-off-by: Emmanuel Nwokoma <nwokoma@gigabytedevelopersinc.com>

--- app/src/main/java/com/gigabytedevelopersinc/app/cometOTP/View/EntryViewHolder.java
+++ app/src/main/java/com/gigabytedevelopersinc/app/cometOTP/View/EntryViewHolder.java
@@ -28,8 +28,7 @@ import me.zhanghai.android.materialprogressbar.MaterialProgressBar;
 
 import static com.gigabytedevelopersinc.app.cometOTP.Activities.MainActivity.animatorDuration;
 
-public class EntryViewHolder extends RecyclerView.ViewHolder
-        implements ItemTouchHelperViewHolder {
+public class EntryViewHolder extends RecyclerView.ViewHolder implements ItemTouchHelperViewHolder {
     private Context context;
     private Callback callback;
     private boolean tapToReveal;
@@ -41,6 +40,8 @@ public class EntryViewHolder extends RecyclerView.ViewHolder
     private FrameLayout thumbnailFrame;
     private ImageView visibleImg;
     private ImageView thumbnailImg;
+    private ImageButton menuButton;
+    private ImageButton copyButton;
     private TextView value;
     private TextView issuer;
     private TextView label;
@@ -50,7 +51,6 @@ public class EntryViewHolder extends RecyclerView.ViewHolder
 
     public EntryViewHolder(Context context, final View v, boolean tapToReveal) {
         super(v);
-
         this.context = context;
 
         card = v.findViewById(R.id.card_view);
@@ -67,8 +67,8 @@ public class EntryViewHolder extends RecyclerView.ViewHolder
         counter = v.findViewById(R.id.counter);
         progressBar = v.findViewById(R.id.cardProgressBar);
 
-        ImageButton menuButton = v.findViewById(R.id.menuButton);
-        ImageButton copyButton = v.findViewById(R.id.copyButton);
+        menuButton = v.findViewById(R.id.menuButton);
+        copyButton = v.findViewById(R.id.copyButton);
         ImageView invisibleImg = v.findViewById(R.id.coverImg);
 
         // Style the buttons in the current theme colors
@@ -131,21 +131,30 @@ public class EntryViewHolder extends RecyclerView.ViewHolder
 
         final String tokenFormatted = Tools.formatToken(entry.getCurrentOTP(), settings.getTokenSplitGroupSize());
 
+        String contentHint = "";
         String issuerText = entry.getIssuer();
+
         if (!TextUtils.isEmpty(issuerText)) {
-            issuer.setText(entry.getIssuer());
+            issuer.setText(issuerText);
             issuer.setVisibility(View.VISIBLE);
+
+            contentHint = issuerText;
         } else {
             issuer.setVisibility(View.GONE);
         }
 
         String labelText = entry.getLabel();
         if (!TextUtils.isEmpty(labelText)) {
-            label.setText(entry.getLabel());
+            label.setText(labelText);
             label.setVisibility(View.VISIBLE);
+
+            contentHint = labelText;
         } else {
             label.setVisibility(View.GONE);
         }
+
+        copyButton.setContentDescription(context.getString(R.string.button_card_copy_format, contentHint));
+        menuButton.setContentDescription(context.getString(R.string.button_card_options_format, contentHint));
 
         value.setText(tokenFormatted);
         // save the unformatted token to the tag of this TextView for copy/paste
@@ -166,7 +175,7 @@ public class EntryViewHolder extends RecyclerView.ViewHolder
         thumbnailFrame.setVisibility(settings.getThumbnailVisible() ? View.VISIBLE : View.GONE);
 
         int thumbnailSize = settings.getThumbnailSize();
-        if(settings.getThumbnailVisible()) {
+        if (settings.getThumbnailVisible()) {
             thumbnailImg.setImageBitmap(EntryThumbnail.getThumbnailGraphic(context, entry.getIssuer(), entry.getLabel(), thumbnailSize, entry.getThumbnail()));
         }
 
