fe6f39fb7d5f34d5f86efce2e604af975b5a1b12
Emmanuel Nwokoma <nwokoma@gigabytedevelopersinc.com>
2020-10-28 05:33:33 +0100

    Change Babiato Forums entry to Babiato
    Signed-off-by: Emmanuel Nwokoma <nwokoma@gigabytedevelopersinc.com>

--- app/src/main/java/com/gigabytedevelopersinc/app/cometOTP/Utilities/EntryThumbnail.java
+++ app/src/main/java/com/gigabytedevelopersinc/app/cometOTP/Utilities/EntryThumbnail.java
@@ -43,7 +43,7 @@ public class EntryThumbnail {
         Autodesk(R.drawable.thumb_autodesk),
         AutoDNS(R.drawable.thumb_autodns),
         AVM(R.drawable.thumb_avm),
-        BabiatoForums(R.drawable.thumb_babiato),
+        Babiato(R.drawable.thumb_babiato),
         Backblaze(R.drawable.thumb_backblaze),
         BattleNet(R.drawable.thumb_battlenet),
         BestBuy(R.drawable.thumb_bestbuy),
