fd14f27dc617404fd50edd5974db39d4012797e8
Emmanuel Nwokoma <nwokoma@gigabytedevelopersinc.com>
2021-04-12 05:02:10 +0100

    Added password length check while creating backup
    Signed-off-by: Emmanuel Nwokoma <nwokoma@gigabytedevelopersinc.com>

--- app/src/main/java/com/gigabytedevelopersinc/app/cometOTP/Dialogs/PasswordEntryDialog.java
+++ app/src/main/java/com/gigabytedevelopersinc/app/cometOTP/Dialogs/PasswordEntryDialog.java
@@ -2,17 +2,21 @@ package com.gigabytedevelopersinc.app.cometOTP.Dialogs;
 
 import android.content.Context;
 
+import com.gigabytedevelopersinc.app.cometOTP.Utilities.ConfirmedPasswordTransformationHelper;
+import com.gigabytedevelopersinc.app.cometOTP.Utilities.Constants;
 import com.gigabytedevelopersinc.app.cometOTP.Utilities.GeneralUtils;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 import androidx.appcompat.app.AppCompatDialog;
 
+import android.os.Build;
 import android.text.Editable;
 import android.text.TextUtils;
 import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.gigabytedevelopersinc.app.cometOTP.R;
 import com.gigabytedevelopersinc.app.cometOTP.Utilities.Tools;
@@ -31,39 +35,42 @@ public class PasswordEntryDialog extends AppCompatDialog
         void onPasswordEntered(String newPassword);
     }
 
-    private Mode dialogMode;
-    private PasswordEnteredCallback callback;
+    private final Mode dialogMode;
+    private final PasswordEnteredCallback callback;
 
-    private TextInputEditText passwordInput;
-    private TextInputLayout passwordLayout;
-    private EditText passwordConfirm;
-    private Button okButton, cancelButton;
+    private final TextInputEditText passwordInput;
+    private final EditText passwordConfirm;
+    private final Button okButton;
+    private final TextView toShortWarning;
 
     public PasswordEntryDialog(Context context, Mode newMode, boolean blockAccessibility, boolean blockAutofill, PasswordEnteredCallback newCallback) {
         super(context, Tools.getThemeResource(context, R.attr.dialogTheme));
 
         setTitle(R.string.dialog_title_enter_password);
         setContentView(R.layout.dialog_password_entry);
-        setCancelable(false);
 
-        passwordLayout = findViewById(R.id.passwordInputLayout);
+        TextInputLayout passwordLayout = findViewById(R.id.passwordInputLayout);
         passwordInput = findViewById(R.id.passwordInput);
         passwordConfirm = findViewById(R.id.passwordConfirm);
+        toShortWarning = findViewById(R.id.toShortWarning);
+        toShortWarning.setText(getContext().getString(R.string.settings_label_short_password, Constants.AUTH_MIN_PASSWORD_LENGTH));
+        ConfirmedPasswordTransformationHelper.setup(passwordLayout, passwordInput, passwordConfirm);
 
         if (blockAccessibility) {
             passwordLayout.setImportantForAccessibility(View.IMPORTANT_FOR_ACCESSIBILITY_NO_HIDE_DESCENDANTS);
             passwordConfirm.setImportantForAccessibility(View.IMPORTANT_FOR_ACCESSIBILITY_NO);
         }
 
-        if (GeneralUtils.INSTANCE.isOreo() && blockAutofill) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O && blockAutofill) {
             passwordLayout.setImportantForAutofill(View.IMPORTANT_FOR_AUTOFILL_NO_EXCLUDE_DESCENDANTS);
             passwordConfirm.setImportantForAutofill(View.IMPORTANT_FOR_AUTOFILL_NO);
         }
 
         okButton = findViewById(R.id.buttonOk);
-        cancelButton = findViewById(R.id.buttonCancel);
-
         okButton.setOnClickListener(this);
+        okButton.setEnabled(false);
+
+        Button cancelButton = findViewById(R.id.buttonCancel);
         cancelButton.setOnClickListener(this);
 
         this.callback = newCallback;
@@ -81,10 +88,15 @@ public class PasswordEntryDialog extends AppCompatDialog
 
     // TextWatcher
     public void onTextChanged(CharSequence s, int start, int before, int count) {
-        if (TextUtils.equals(passwordInput.getEditableText(), passwordConfirm.getEditableText()))
-            okButton.setEnabled(true);
-        else
+        if (passwordInput.getEditableText().length() >= Constants.AUTH_MIN_PASSWORD_LENGTH) {
+            toShortWarning.setVisibility(View.GONE);
+            if (TextUtils.equals(passwordInput.getEditableText(), passwordConfirm.getEditableText()))
+                okButton.setEnabled(true);
+        }
+        else {
+            toShortWarning.setVisibility(View.VISIBLE);
             okButton.setEnabled(false);
+        }
     }
 
     public void afterTextChanged(Editable s) {}
@@ -94,9 +106,9 @@ public class PasswordEntryDialog extends AppCompatDialog
     public void onClick(View view)  {
         if (view.getId() == R.id.buttonOk) {
             if (callback != null)
-                callback.onPasswordEntered(Objects.requireNonNull(passwordInput.getText()).toString());
-            }
+                callback.onPasswordEntered(passwordInput.getText().toString());
+        }
 
-            dismiss();
+        dismiss();
     }
 }
