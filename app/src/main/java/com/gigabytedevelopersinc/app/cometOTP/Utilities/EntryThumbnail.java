e59a43a3132ac09a341ffd1e7c910f0dabb53fb5
Emmanuel Nwokoma <nwokoma@gigabytedevelopersinc.com>
2021-04-12 23:33:06 +0100

    Added account thumbnail for OneBlu
    Signed-off-by: Emmanuel Nwokoma <nwokoma@gigabytedevelopersinc.com>

--- app/src/main/java/com/gigabytedevelopersinc/app/cometOTP/Utilities/EntryThumbnail.java
+++ app/src/main/java/com/gigabytedevelopersinc/app/cometOTP/Utilities/EntryThumbnail.java
@@ -21,6 +21,7 @@ public class EntryThumbnail {
     public enum EntryThumbnails {
         Default(R.mipmap.ic_launcher_round),
         OneAndOne(R.drawable.thumb_1and1),
+        OneBlu(R.drawable.thumb_1blu),
         OnePassword(R.drawable.thumb_1password),
         OneXBet(R.drawable.thumb_1xbet),
         TwentyThreeAndMe(R.drawable.thumb_23andme),
