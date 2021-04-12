bd520c005e535768c3ff5b9669c9fc193d505361
Emmanuel Nwokoma <nwokoma@gigabytedevelopersinc.com>
2021-04-12 21:31:43 +0100

    Added account thumbnail for Grammarly
    Signed-off-by: Emmanuel Nwokoma <nwokoma@gigabytedevelopersinc.com>

--- app/src/main/java/com/gigabytedevelopersinc/app/cometOTP/Utilities/EntryThumbnail.java
+++ app/src/main/java/com/gigabytedevelopersinc/app/cometOTP/Utilities/EntryThumbnail.java
@@ -136,6 +136,7 @@ public class EntryThumbnail {
         Gogs(R.drawable.thumb_gogs),
         Google(R.drawable.thumb_google),
         GovUK(R.drawable.thumb_govuk),
+        Grammarly(R.drawable.thumb_grammarly),
         Greenhost(R.drawable.thumb_greenhost),
         HackerOne(R.drawable.thumb_hackerone),
         Heroku(R.drawable.thumb_heroku),
