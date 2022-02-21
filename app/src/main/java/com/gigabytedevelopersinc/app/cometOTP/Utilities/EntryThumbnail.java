733bbacd4aedd9aa010e21c559b78439016d06e6
Emmanuel Nwokoma <nwokoma@gigabytedevelopersinc.com>
2022-02-21 01:56:45 +0100

    Added ServiceNow account thumbnail
    Signed-off-by: Emmanuel Nwokoma <nwokoma@gigabytedevelopersinc.com>

--- app/src/main/java/com/gigabytedevelopersinc/app/cometOTP/Utilities/EntryThumbnail.java
+++ app/src/main/java/com/gigabytedevelopersinc/app/cometOTP/Utilities/EntryThumbnail.java
@@ -311,6 +311,7 @@ public class EntryThumbnail {
         SelfWealth(R.drawable.thumb_selfwealth),
         School(R.drawable.thumb_school),
         Sentry(R.drawable.thumb_sentry),
+        ServiceNow(R.drawable.thumb_servicenow),
         Sevdesk(R.drawable.thumb_sevdesk),
         SimpleLogin(R.drawable.thumb_simple_login),
         Skrill(R.drawable.thumb_skrill),
