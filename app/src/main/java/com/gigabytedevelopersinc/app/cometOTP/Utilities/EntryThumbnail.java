81154d80aba9566d04679b9b947eaf0846892842
Emmanuel Nwokoma <nwokoma@gigabytedevelopersinc.com>
2022-05-27 02:07:18 +0100

    Added Shoppe.com account thumbnail
    Signed-off-by: Emmanuel Nwokoma <nwokoma@gigabytedevelopersinc.com>

--- app/src/main/java/com/gigabytedevelopersinc/app/cometOTP/Utilities/EntryThumbnail.java
+++ app/src/main/java/com/gigabytedevelopersinc/app/cometOTP/Utilities/EntryThumbnail.java
@@ -322,6 +322,7 @@ public class EntryThumbnail {
         ServiceNow(R.drawable.thumb_servicenow),
         Sevdesk(R.drawable.thumb_sevdesk),
         SimpleLogin(R.drawable.thumb_simple_login),
+        ShoppeeCom(R.drawable.thumb_shoppee_com),
         Skrill(R.drawable.thumb_skrill),
         Slack(R.drawable.thumb_slack),
         Smartbill(R.drawable.thumb_smartbill),
